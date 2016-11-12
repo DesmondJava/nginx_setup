636faf4bba805c596cd4733dcfa7b8488b4bcdce
Vadym Shevchenko <spawnkiev@gmail.com>
2016-11-12 17:55:51 +0200

    add new

--- etc/hello.py
+++
@@ -1,5 +0,0 @@
-import multiprocessing
-
-pythonpath = '/home/box/web'
-bind = "0.0.0.0:8080"
-workers = multiprocessing.cpu_count() * 2 + 1
--- init.sh
+++ init.sh
@@ -1,6 +1,27 @@
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-sudo /etc/init.d/nginx restart
-sudo pkill gunicorn
-gunicorn -b 0.0.0.0:8080 hello:app &
-gunicorn -b 0.0.0.0:8000 /home/box/web/ask ask.wsgi:application &
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]
+then
+	echo "Changing nginx config..."
+	sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+	echo "Stopping nginx..."
+	sudo /etc/init.d/nginx stop
+	echo "Starting nginx again..."
+	sudo /usr/sbin/nginx -c /etc/nginx/sites-enabled/test.conf
+	echo "Changing Gunicorn config..."
+	sudo ln -sf /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/test
+	echo "Restarting gunicorn..."
+	cd ~/web/ask
+	sudo gunicorn -c /etc/gunicorn.d/test wsgi:application &
+        # gunicorn -c ./etc/gunicorn.conf --chdir ask/ ask.wsgi
+elif [[ "$unamestr" == 'vadym' ]]
+then
+	echo "Changing nginx config..."
+	sudo ln -sf $HOME/workspace/projects/stepic/stepic-django/etc/nginx.conf  /usr/local/etc/nginx/test.conf
+	echo "Stopping nginx..."
+	sudo nginx -s stop
+	echo "Starting nginx again..."
+	sudo nginx -c /usr/local/etc/nginx/test.conf
+
+	echo "Starting gunicorn..."
+	sudo gunicorn -c ./etc/gunicorn.conf --chdir ask/ ask.wsgi
+fi
