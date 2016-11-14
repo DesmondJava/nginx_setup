f16c1a8457312e398e01df68f0511c28a3d2f259
Vadym Shevchenko <SpawnKiev@gmail.com>
2016-11-14 11:17:34 +0200

    Update init.sh
    add descriptions to my init file

--- init.sh
+++ init.sh
@@ -1,7 +1,19 @@
+#!/bin/bash
+echo "Setup NGINX and GUNICORN on this machine..."
+echo "Create soft links to the /etc/ path nginx and gunicorn for setup config."
 sudo ln -sf ~/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo ln -sf ~/web/etc/hello.py /etc/gunicorn.d/hello.py
+
+echo "Restart NGINX with new config."
 sudo /etc/init.d/nginx restart
+
+echo "Kill all previous jobs Gunicorn's for run apps with new configs"
 sudo pkill gunicorn
+
+echo "Run hello application on 0.0.0.0:8080!"
 gunicorn -b 0.0.0.0:8080 hello:app &
+
+echo "Go to the path ~/web/ask"
 cd ~/web/ask
+echo "Run ask application on 0.0.0.0:8000!"
 gunicorn -b 0.0.0.0:8000 ask.wsgi:application &
