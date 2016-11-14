e11fe15816a5ff8d2875aefeb5e337bf4dce799e
Vadym Shevchenko <SpawnKiev@gmail.com>
2016-11-14 11:12:13 +0200

    Update gitinit.sh

--- gitinit.sh
+++ gitinit.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
+# setup Git globaly
+echo "Setup Git globaly..."
 git config --global user.email "spawnkiev@gmail.com"
 git config --global user.name "Vadym Shevchenko"
+echo "Successfully done!"
