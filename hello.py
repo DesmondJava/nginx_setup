df0606e8cfbc46f1b27460a57d96d4980b711334
Vadym Shevchenko <SpawnKiev@gmail.com>
2016-11-14 11:09:33 +0200

    Update hello.py
    edit file

--- hello.py
+++ hello.py
@@ -1,16 +1,14 @@
 from cgi import parse_qs, escape
 
 def app(environ, start_response):
-    string = environ['QUERY_STRING']
-    
-    status = '200 OK'
-    
+    qs = environ['QUERY_STRING']
+    response_status = '200 OK'
     response_headers = [('Content-Type', 'text/plain')]
 
     start_response(status, response_headers)
-    resp = string.split('&')
+    response = qs.split('&')
     body = ''
-    for item in resp:
+    for item in response:
         body += item + '\n'
 
     return [body]
