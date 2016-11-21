91cf2867fac9aed41ef5a6154b0369cfacbb931b
Vadym Shevchenko <vshevche@cisco.com>
2016-11-21 16:58:36 +0200

    test

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -108,5 +108,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = '/home/vadym/web/ask/static/'
+STATIC_URL = '/static/'
 # STATIC_ROOT = '/home/vadym/web/ask/static/'
