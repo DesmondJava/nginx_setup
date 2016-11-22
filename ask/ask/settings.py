e594bcd1dcc4a7945f34222b984666396e067bfa
Vadym Shevchenko <spawnkiev@gmail.com>
2016-11-22 11:06:56 +0200

    a lot of changes

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -33,6 +33,9 @@ STATICFILES_DIRS = [
 os.path.join(BASE_DIR, "qa", "static"),
 ]
 
+if __name__ == '__main__':
+    print BASE_DIR
+    print STATICFILES_DIRS
 # Application definition
 
 INSTALLED_APPS = (
