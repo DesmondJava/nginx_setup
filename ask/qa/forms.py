0ac3e40f53d59e43046f0c8b92c00e28bc573301
Vadym Shevchenko <vshevche@cisco.com>
2016-11-25 12:57:35 +0200

    test

--- ask/qa/forms.py
+++ ask/qa/forms.py
@@ -8,9 +8,9 @@ from qa.models import Question, Answer, User
 
 
 class AskForm(forms.Form):
-    title = forms.CharField(label="Title", max_length=30,
+    title = forms.CharField(label="Title", max_length=100,
                             widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'title'}))
-    text = forms.CharField(label="Question", max_length=30,
+    text = forms.CharField(label="Question", max_length=500,
                            widget=forms.Textarea(attrs={'class': 'form-control', 'name': 'text'}))
 
     def clean_title(self):
@@ -36,7 +36,7 @@ class AskForm(forms.Form):
 
 
 class AnswerForm(forms.Form):
-    text = forms.CharField(label="Answer", max_length=30,
+    text = forms.CharField(label="Answer", max_length=150,
                            widget=forms.Textarea(attrs={'class': 'form-control', 'name': 'text'}))
     question = forms.IntegerField(widget=forms.HiddenInput)
 
